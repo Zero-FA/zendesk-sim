f8c433be4feca2156e5d7a2d2e53d791f88b2265
Sean Michael <michaelmat67@gmail.com>
2025-08-13 17:44:21 -0400

    Delete api/scenarios.js

--- api/scenarios.js
+++
@@ -1,7 +0,0 @@
-// api/scenarios.js
-import scenarios from '../data/scenarios.json' assert { type: 'json' };
-
-export default function handler(req, res) {
-  // You can filter fields here to hide trainer-only data from trainees if needed.
-  res.status(200).json(scenarios);
-}
